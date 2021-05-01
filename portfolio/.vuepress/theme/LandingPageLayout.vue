55c27d81a2b6348449af67fb849a9670ca3c9342
Joe Qureshi <joe.qureshi@icloud.com>
2021-05-01 18:00:55 +0100

    Init

---
+++ portfolio/.vuepress/theme/LandingPageLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <Navbar :logo="$site.themeConfig.logo" :sticky="$route.path === '/'" />
+</template>
+
+<script>
+export default {
+  name: "LandingPageLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
